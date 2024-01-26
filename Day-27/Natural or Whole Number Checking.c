8f4a5a02409fe7b4638910befb0ee37fd9177762
PrabinDumre <116421340+PrabinDumre@users.noreply.github.com>
2024-01-26 23:00:18 +0530

    Checking whether input is natural / whole or not

---
+++ Day-27/Natural or Whole Number Checking.c
@@ -0,0 +1,26 @@
+Natural Number
+int main(){
+  int n;
+  printf("enter a number : ");
+  scanf("%d",&n);
+  if(n>0){
+    printf("The number is natural number");
+  } else {
+    printf("The number isnot natural number");
+  }
+    return 0;
+}
+
+Whole Number
+
+int main(){
+  int n;
+  printf("enter a number : ");
+  scanf("%d",&n);
+  if(n=>0){
+    printf("The number is whole number");
+  } else {
+    printf("The number isnot whole number");
+  }
+    return 0;
+}
